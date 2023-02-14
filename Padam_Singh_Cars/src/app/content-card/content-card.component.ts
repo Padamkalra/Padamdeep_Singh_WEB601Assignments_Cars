a70be596c01cffa75d184a6534d2dcbe175c28f2
Padamkalra <105304950+Padamkalra@users.noreply.github.com>
2023-02-13 23:11:23 -0500

    updated the filles

--- Padam_Singh_Cars/src/app/content-card/content-card.component.ts
+++ Padam_Singh_Cars/src/app/content-card/content-card.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
@@ -8,37 +7,4 @@ import { ContentList } from '../helper-files/content-list';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent {
-  myContentList : ContentList = new ContentList();
-
-  contentItem1 : Content = {
-    id: 0,
-    title: 'abc',
-    description: 'nvhjvfjhSB',
-    creator: 'dxggf'
-  }
-  contentItem2 : Content = {
-    id: 0,
-    title: 'sdf',
-    description: 'bvhjsbdug',
-    creator: 'xfxcv'
-  }
-  contentItem3 : Content = {
-    id: 0,
-    title: 'fgh',
-    description: 'dbjdskfhi',
-    creator: 'Creator3'
-  }
-  constructor(){
-    this.myContentList.addFunction(this.contentItem1);
-    this.myContentList.addFunction(this.contentItem2);
-    this.myContentList.addFunction(this.contentItem3);
-  }
-
-  displayMyIndex(index : number) {
-    let myItems = this.myContentList.getItems();
-
-    if (myItems.length > 0 && index > 0 && index < myItems.length){
-      return this.myContentList.printIndex(index);
-    }
-  }
 }
