7191b52bfd311c81f97375aad55c413634ea6406
Padamkalra <105304950+Padamkalra@users.noreply.github.com>
2023-02-27 17:50:01 -0500

    updated

--- Padam_Singh_Cars/src/app/helper-files/content-interface.ts
+++ Padam_Singh_Cars/src/app/helper-files/content-interface.ts
@@ -1,9 +1,9 @@
-export interface Content {
-    id: number;
-    title: string;
-    description: string;
-    imgURL?: string;
-    type?: string;
-    tags?: string [];
-    creator: string;
+export interface Content{
+    id: number,
+    title: string,
+    description: string,
+    creator: string,
+    imgURL?: string,
+    type?: string,
+    tags?: string[]
 }
