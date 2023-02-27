5b7135c18138f337284e97020dd1d133aac5d18a
Padamkalra <105304950+Padamkalra@users.noreply.github.com>
2023-02-27 18:09:10 -0500

    updated the files

--- Padam_Singh_Cars/src/app/app.module.ts
+++ Padam_Singh_Cars/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
+import { ContentTypePipe } from './pipes/content-type.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
-    ContentListComponent
+    ContentListComponent,
+    ContentTypePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
