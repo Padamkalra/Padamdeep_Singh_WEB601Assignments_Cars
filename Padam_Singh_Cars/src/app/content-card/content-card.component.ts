b4a1b70ac35b21bee7020ce8382c9b51935e741a
Padamkalra <105304950+Padamkalra@users.noreply.github.com>
2023-02-27 17:48:50 -0500

    assignment 3 started

---
+++ Padam_Singh_Cars/src/app/content-card/content-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss']
+})
+export class ContentCardComponent {
+}
