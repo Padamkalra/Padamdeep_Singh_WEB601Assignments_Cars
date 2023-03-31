35ea9b5749a43a789126c4b47b45dd6a6a2f1569
Padamkalra <105304950+Padamkalra@users.noreply.github.com>
2023-03-30 21:40:37 -0400

    updated

--- Padam_Singh_Cars/src/app/app.module.ts
+++ Padam_Singh_Cars/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentTypePipe } from './pipes/content-type.pipe';
+import { HoverAffectDirectiveSpecTsDirective } from './directives/hover-affect.directive.spec.ts.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
     ContentListComponent,
-    ContentTypePipe
+    ContentTypePipe,
+    HoverAffectDirectiveSpecTsDirective
   ],
   imports: [
     BrowserModule,
---
+++ Padam_Singh_Cars/src/app/directives/hover-affect.directive.spec.ts.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HoverAffectDirectiveSpecTsDirective } from './hover-affect.directive.spec.ts.directive';
+
+describe('HoverAffectDirectiveSpecTsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HoverAffectDirectiveSpecTsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
